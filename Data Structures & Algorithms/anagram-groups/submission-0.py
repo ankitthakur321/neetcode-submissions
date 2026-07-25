94c0d469ced7051d0204fca92b89dc7ddf2430b2
Ankit Kumar <49100744+ankitthakur321@users.noreply.github.com>
2026-07-25 11:56:36 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        resList = []
+        innerList = []
+        addedList = []
+        i = 0
+        for i in range(len(strs)):
+            innerList = [strs[i]]
+            for j in range(i+1, len(strs)):
+                if sorted(strs[i]) == sorted(strs[j]):
+                    innerList.append(strs[j])
+            if not any(item in sublist for sublist in resList for item in innerList):
+                resList.append(innerList)
+        return sorted(resList)
+
+        
